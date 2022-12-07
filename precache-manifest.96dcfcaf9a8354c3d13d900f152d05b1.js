fa5f436aea0c208aba95c0bf34c0bc38ad1634a9
Jamison Wight <jamisonw.wight@gmail.com>
2022-12-07 14:34:05 -0500

    Update home title alignment main

---
+++ precache-manifest.96dcfcaf9a8354c3d13d900f152d05b1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f1ca36ea381e99705c",
+    "url": "/css/app.845d72e9.css"
+  },
+  {
+    "revision": "85ee26305da2ed85dd5d",
+    "url": "/css/chunk-vendors.df3ea0ce.css"
+  },
+  {
+    "revision": "164c2cc8db51cfaf86d40a31c6b6577e",
+    "url": "/img/android.164c2cc8.svg"
+  },
+  {
+    "revision": "ce842fa16f1a9b34fc5a96b34cf6f4cd",
+    "url": "/img/arrow-left-dark-icon.ce842fa1.svg"
+  },
+  {
+    "revision": "9b68a198fd3fd14f7bf3c6bff83588c6",
+    "url": "/img/arrow-right-icon.9b68a198.svg"
+  },
+  {
+    "revision": "8422c01e6035115335c5607085780f63",
+    "url": "/img/background.8422c01e.svg"
+  },
+  {
+    "revision": "9ec8649fc4c29c70fd4eaa253245e09b",
+    "url": "/img/date-night-icon.9ec8649f.svg"
+  },
+  {
+    "revision": "53d17f456ca217e2f2c08b99c27d41c0",
+    "url": "/img/date-night-yellow-icon.53d17f45.svg"
+  },
+  {
+    "revision": "3f4ce9b202430e2d419614ffb65fa296",
+    "url": "/img/family-friendly-icon.3f4ce9b2.svg"
+  },
+  {
+    "revision": "85049798df8a90d7c1af5cfbbeaf85e7",
+    "url": "/img/family-friendly-yellow-icon.85049798.svg"
+  },
+  {
+    "revision": "37af862392f4273d2d8a6dac1d6ff54e",
+    "url": "/img/girls-night-icon.37af8623.svg"
+  },
+  {
+    "revision": "8702590434d1f1f9821dcb5b1d7139c9",
+    "url": "/img/girls-night-yellow-icon.87025904.svg"
+  },
+  {
+    "revision": "d8da924566292dbcc0f46bc17f4a4984",
+    "url": "/img/heart-black.d8da9245.svg"
+  },
+  {
+    "revision": "539e61b8e4517f1cc867c7e14a5c8a32",
+    "url": "/img/heart-red.539e61b8.svg"
+  },
+  {
+    "revision": "3368960437d0ea0ee31e102a37bf61b2",
+    "url": "/img/home-small.33689604.svg"
+  },
+  {
+    "revision": "3ddedeba3b6420e66ce1517a01d6593e",
+    "url": "/img/ios.3ddedeba.svg"
+  },
+  {
+    "revision": "cff62bec3592a15a821861621bcd3aad",
+    "url": "/img/logo.cff62bec.svg"
+  },
+  {
+    "revision": "2ec1cd95f15d7e7bf5272741684f8ddf",
+    "url": "/img/movie-night-bg.2ec1cd95.svg"
+  },
+  {
+    "revision": "dc59cf5117e88782e24d63d03fc20225",
+    "url": "/img/movie-night-icon.dc59cf51.svg"
+  },
+  {
+    "revision": "ff762411c13918bf8b86583e4364785e",
+    "url": "/img/movie-night-yellow-icon.ff762411.svg"
+  },
+  {
+    "revision": "26fa39a3542d27b80e88ae8749bd6388",
+    "url": "/img/squiggly.26fa39a3.svg"
+  },
+  {
+    "revision": "212c8bf9e448b11e627844e94d909765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f1ca36ea381e99705c",
+    "url": "/js/app.010127c4.js"
+  },
+  {
+    "revision": "85ee26305da2ed85dd5d",
+    "url": "/js/chunk-vendors.d03ea913.js"
+  },
+  {
+    "revision": "c55eb36d4c592db06f22ebff78d2a6d6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
